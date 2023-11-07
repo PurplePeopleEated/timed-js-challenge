f438d16c7a07e221f3f8eb8b3580a1b212bca742
Karra <karra.maynard@gmail.com>
2023-11-06 22:39:30 -0500

    add button vars

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Button variables
+var buttonA = document.getElementById('btnA');
+var buttonB = document.getElementById('btnB');
+var buttonC = document.getElementById('btnC');
+var buttonD = document.getElementById('btnD');
+var startButton = document.getElementById('btn-start');
+var backButton = document.getElementById('btn-back');
