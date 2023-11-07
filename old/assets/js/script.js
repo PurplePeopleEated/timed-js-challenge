2a01efa37de030e3e4a081c0b4b889bddfcc2c23
Karra <karra.maynard@gmail.com>
2023-11-06 22:34:16 -0500

    create script.js

--- old/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// Buttons
-var buttonA = document.getElementById('btnA');
-var buttonB = document.getElementById('btnB');
-var buttonC = document.getElementById('btnC');
-var buttonD = document.getElementById('btnD');
-var startButton = document.getElementById('btn-start');
-var backButton = document.getElementById('btn-back');
