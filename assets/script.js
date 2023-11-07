e0b34dbaf7e0deb715a8d14210ae88b57d0da41a
Karra <karra.maynard@gmail.com>
2023-11-06 23:27:13 -0500

    create init function

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,25 @@ var buttonC = document.getElementById('btnC');
 var buttonD = document.getElementById('btnD');
 var startButton = document.getElementById('btn-start');
 var backButton = document.getElementById('btn-back');
+
+// Arrays for questions, answers, and fake answers
+const allQuestions = [
+  {'question': 'This is question number 1?', 
+  'answer': 'Correct1'}, 
+
+  {'question': 'This is question number 2?', 
+  'answer': 'Correct2'}, 
+
+  {'question': 'This is question number 3?', 
+  'answer': 'Correct3'},
+
+  {'question': 'This is question number 4?', 
+  'answer': 'Correct4'}];
+
+const fakeAnswers = ['whoo', 'yippie', 'gawrsh', 'cinnamon', 'crazy', 'haus', 'woah'];
+
+// Runs on page load
+function init() {
+  questionCard.innerHTML = 'Timed JavaScript Challenge';
+  answersEl.style.display = 'none';
+}
